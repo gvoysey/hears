ede66becff85df3ec41155fb64a8652830e93f25
Graham Voysey <gvoysey@bu.edu>
2017-06-29 21:14:31 -0400

    import fixing

--- hears/filtering/firfilterbank.py
+++ hears/filtering/firfilterbank.py
@@ -1,15 +1,19 @@
-'''
+"""
 FIR filterbank, can be treated as a special case of LinearFilterbank, but an
 optimisation is possible using buffered output by using FFT based convolution
 as in HRTF.apply. To do this is slightly tricky because it needs to cache
 previous inputs. For the moment, we implement it as a special case of
 LinearFilterbank but later this will change to using the FFT method.
-'''
-from brian import *
+"""
+from brian2 import *
 from hears.filtering.filterbank import *
 from hears.filtering.linearfilterbank import *
+import numpy as np
+from numpy.fft import fft
+from math import ceil, log2
 
 __all__ = ['FIRFilterbank', 'LinearFIRFilterbank', 'FFTFIRFilterbank']
+
 
 class LinearFIRFilterbank(LinearFilterbank):
     def __init__(self, source, impulse_response, minimum_buffer_size=None):
@@ -19,20 +23,22 @@ class LinearFIRFilterbank(LinearFilterbank):
         # just once when we move to using FFT based convolution and in fact this
         # will save a lot of computation as the FFT only needs to be computed
         # once then.
-        if len(impulse_response.shape)==1:
+        if len(impulse_response.shape) == 1:
             impulse_response = repeat(reshape(impulse_response, (1, len(impulse_response))), source.nchannels, axis=0)
         # Automatically duplicate mono input to fit the desired output shape
-        if impulse_response.shape[0]!=source.nchannels:
-            if source.nchannels!=1:
-                raise ValueError('Can only automatically duplicate source channels for mono sources, use RestructureFilterbank.')
+        if impulse_response.shape[0] != source.nchannels:
+            if source.nchannels != 1:
+                raise ValueError(
+                    'Can only automatically duplicate source channels for mono sources, use RestructureFilterbank.')
             source = RestructureFilterbank(source, impulse_response.shape[0])
         # Implement it as a LinearFilterbank
-        b = reshape(impulse_response, impulse_response.shape+(1,))
+        b = reshape(impulse_response, impulse_response.shape + (1,))
         a = zeros_like(b)
         a[:, 0, :] = 1
         LinearFilterbank.__init__(self, source, b, a)
         if minimum_buffer_size is not None:
             self.minimum_buffer_size = minimum_buffer_size
+
 
 class FFTFIRFilterbank(Filterbank):
     def __init__(self, source, impulse_response, minimum_buffer_size=None):
@@ -42,90 +48,92 @@ class FFTFIRFilterbank(Filterbank):
         # just once when we move to using FFT based convolution and in fact this
         # will save a lot of computation as the FFT only needs to be computed
         # once then.
-        if len(impulse_response.shape)==1:
+        if len(impulse_response.shape) == 1:
             impulse_response = repeat(reshape(impulse_response, (1, len(impulse_response))), source.nchannels, axis=0)
         # Automatically duplicate mono input to fit the desired output shape
-        if impulse_response.shape[0]!=source.nchannels:
-            if source.nchannels!=1:
-                raise ValueError('Can only automatically duplicate source channels for mono sources, use RestructureFilterbank.')
+        if impulse_response.shape[0] != source.nchannels:
+            if source.nchannels != 1:
+                raise ValueError(
+                    'Can only automatically duplicate source channels for mono sources, use RestructureFilterbank.')
             source = RestructureFilterbank(source, impulse_response.shape[0])
         Filterbank.__init__(self, source)
 
-        self.input_cache = zeros((impulse_response.shape[1], self.nchannels))
+        self.input_cache = np.zeros((impulse_response.shape[1], self.nchannels))
         self.impulse_response = impulse_response
         self.fftcache_nmax = -1
         if minimum_buffer_size is None:
-            minimum_buffer_size = 3*impulse_response.shape[1]
+            minimum_buffer_size = 3 * impulse_response.shape[1]
         self.minimum_buffer_size = minimum_buffer_size
 
     def buffer_init(self):
         Filterbank.buffer_init(self)
         self.input_cache[:] = 0
 
-    # This version uses a single FFT/IFFT call, using the axis keyword, but it
-    # doesn't appear to be any more efficient than looping, and uses much more
-    # memory, although my tests weren't exhaustive.
-#    def buffer_apply(self, input):
-#        nmax = max(self.input_cache.shape[0]+input.shape[0], self.impulse_response.shape[1])
-#        nmax = 2**int(ceil(log2(nmax)))
-#        if self.fftcache_nmax!=nmax:
-#            # impulse response: (nchannels, ir_length)
-#            ir = zeros((self.nchannels, nmax))
-#            ir[:, :self.impulse_response.shape[1]] = self.impulse_response
-#            # fftcache: (ir_length, nchannels)
-#            self.fftcache = fft(ir, n=nmax, axis=1).T
-#            self.fftcache_nmax = nmax
-#        fullinput = vstack((self.input_cache, input))
-#        fullinput = vstack((fullinput, zeros((nmax-fullinput.shape[1], self.nchannels))))
-#        # fullinput: (ir_length, nchannels)
-#        fullinput_fft = fft(fullinput, n=nmax, axis=0)
-#        fulloutput_fft = fullinput_fft*self.fftcache
-#        fulloutput = ifft(fulloutput_fft, n=nmax, axis=0).real
-#        output = fulloutput[self.input_cache.shape[0]:self.input_cache.shape[0]+input.shape[0]]
-#        # update input cache
-#        nic = self.input_cache.shape[0]
-#        ni = input.shape[0]
-#        #print ni, nic
-#        if ni>=nic:
-#            self.input_cache[:, :] = input[-nic:, :]
-#        else:
-#            self.input_cache[:-ni, :] = self.input_cache[ni:, :]
-#            self.input_cache[-ni:, :] = input
-#        return output
-    
+        # This version uses a single FFT/IFFT call, using the axis keyword, but it
+        # doesn't appear to be any more efficient than looping, and uses much more
+        # memory, although my tests weren't exhaustive.
+
+    #    def buffer_apply(self, input):
+    #        nmax = max(self.input_cache.shape[0]+input.shape[0], self.impulse_response.shape[1])
+    #        nmax = 2**int(ceil(log2(nmax)))
+    #        if self.fftcache_nmax!=nmax:
+    #            # impulse response: (nchannels, ir_length)
+    #            ir = zeros((self.nchannels, nmax))
+    #            ir[:, :self.impulse_response.shape[1]] = self.impulse_response
+    #            # fftcache: (ir_length, nchannels)
+    #            self.fftcache = fft(ir, n=nmax, axis=1).T
+    #            self.fftcache_nmax = nmax
+    #        fullinput = vstack((self.input_cache, input))
+    #        fullinput = vstack((fullinput, zeros((nmax-fullinput.shape[1], self.nchannels))))
+    #        # fullinput: (ir_length, nchannels)
+    #        fullinput_fft = fft(fullinput, n=nmax, axis=0)
+    #        fulloutput_fft = fullinput_fft*self.fftcache
+    #        fulloutput = ifft(fulloutput_fft, n=nmax, axis=0).real
+    #        output = fulloutput[self.input_cache.shape[0]:self.input_cache.shape[0]+input.shape[0]]
+    #        # update input cache
+    #        nic = self.input_cache.shape[0]
+    #        ni = input.shape[0]
+    #        #print ni, nic
+    #        if ni>=nic:
+    #            self.input_cache[:, :] = input[-nic:, :]
+    #        else:
+    #            self.input_cache[:-ni, :] = self.input_cache[ni:, :]
+    #            self.input_cache[-ni:, :] = input
+    #        return output
+
     def buffer_apply(self, input):
         output = zeros_like(input)
-        nmax = max(self.input_cache.shape[0]+input.shape[0], self.impulse_response.shape[1])
-        nmax = 2**int(ceil(log2(nmax)))
-        if self.fftcache_nmax!=nmax:
+        nmax = max(self.input_cache.shape[0] + input.shape[0], self.impulse_response.shape[1])
+        nmax = 2 ** int(ceil(log2(nmax)))
+        if self.fftcache_nmax != nmax:
             self.fftcache = []
         for i, (previnput, curinput, ir) in enumerate(zip(self.input_cache.T,
                                                           input.T,
                                                           self.impulse_response)):
             fullinput = hstack((previnput, curinput))
             # pad
-            fullinput = hstack((fullinput, zeros(nmax-len(fullinput))))
+            fullinput = hstack((fullinput, np.zeros(nmax - len(fullinput))))
             # apply fft
-            if self.fftcache_nmax!=nmax:
+            if self.fftcache_nmax != nmax:
                 # recompute IR fft, first pad, then take fft, then store
-                ir = hstack((ir, zeros(nmax-len(ir))))
+                ir = hstack((ir, np.zeros(nmax - len(ir))))
                 ir_fft = fft(ir, n=nmax)
                 self.fftcache.append(ir_fft)
             else:
                 ir_fft = self.fftcache[i]
             fullinput_fft = fft(fullinput, n=nmax)
-            curoutput_fft = fullinput_fft*ir_fft
+            curoutput_fft = fullinput_fft * ir_fft
             curoutput = ifft(curoutput_fft)
             # unpad
-            curoutput = curoutput[len(previnput):len(previnput)+len(curinput)]
+            curoutput = curoutput[len(previnput):len(previnput) + len(curinput)]
             output[:, i] = curoutput.real
-        if self.fftcache_nmax!=nmax:
+        if self.fftcache_nmax != nmax:
             self.fftcache_nmax = nmax
         # update input cache
         nic = self.input_cache.shape[0]
         ni = input.shape[0]
-        #print ni, nic
-        if ni>=nic:
+        # print ni, nic
+        if ni >= nic:
             self.input_cache[:, :] = input[-nic:, :]
         else:
             self.input_cache[:-ni, :] = self.input_cache[ni:, :]
@@ -134,7 +142,7 @@ class FFTFIRFilterbank(Filterbank):
 
 
 class FIRFilterbank(Filterbank):
-    '''
+    """
     Finite impulse response filterbank
     
     Initialisation parameters:
@@ -154,7 +162,8 @@ class FIRFilterbank(Filterbank):
         ``buffer_size+ir_length`` should be a power of 2 (otherwise there will
         be some zero padding), and ``buffer_size`` should be as large as
         possible.
-    '''
+    """
+
     def __init__(self, source, impulse_response, use_linearfilterbank=False,
                  minimum_buffer_size=None):
         if use_linearfilterbank:
